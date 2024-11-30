0857b177340c49f690a782075371b3a534b46538
Tim G. Thomas <github@timgthomas.com>
2024-11-29 23:39:07 -0600

    fix some typing

--- src/routes/(index)/+page.server.ts
+++ src/routes/(index)/+page.server.ts
@@ -1,6 +1,6 @@
 import Post from '$lib/server/models/post'
-import type { PageLoad } from './$types'
+import type { PageServerLoad } from './$types'
 
-export const load: PageLoad = async () => {
+export const load: PageServerLoad = async () => {
   return { post: (await Post.getAll())[0] }
 }
