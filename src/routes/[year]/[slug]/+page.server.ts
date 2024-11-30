aa19b60e5d2438a0317f94b606f250ddb9d4eecf
Tim G. Thomas <github@timgthomas.com>
2024-11-29 22:44:32 -0600

    add links to posts

--- src/routes/[year]/[slug]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { error } from '@sveltejs/kit'
-import Post from '$lib/server/models/post'
-import type { PageServerLoad } from './$types'
-
-export const load: PageServerLoad = async ({ params }) => {
-  const postPath = `/${params.year}/${params.slug}.md`
-  const post = await Post.get(postPath)
-  return post ?? error(404, 'Not found')
-}
--- src/routes/archives/+page.svelte
+++ src/routes/archives/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
+  import type Post from '$lib/server/models/post'
   import type { PageData } from './$types'
 
   const { data }: { data: PageData } = $props()
+
+  function urlFor(post: Post): string {
+    const [month, , ...slugParts] = post.slug.split('-')
+    return `/${post.year}/${month}/${slugParts.join('-')}`
+  }
 </script>
 
 <ol>
   {#each data.posts as post}
-    <li>{post.title}</li>
+    <li>
+      <a href={urlFor(post)}>{post.title}</a>
+    </li>
   {/each}
 </ol>
