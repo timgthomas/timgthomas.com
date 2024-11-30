32fd2b4e6d81d369e7b46e961408c191c6b22b89
Tim G. Thomas <github@timgthomas.com>
2024-11-29 23:56:10 -0600

    ah ha! reduce page bloat from an errant layout fetch

---
+++ src/routes/archives/+page.server.ts
@@ -0,0 +1,7 @@
+import Post from '$lib/server/models/post'
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async () => {
+  const posts = await Post.getAll()
+  return { posts }
+}
