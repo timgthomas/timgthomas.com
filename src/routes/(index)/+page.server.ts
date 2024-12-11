c1e32c5294ba0fb4461c322f00d4d5fafb4b24cd
Tim G. Thomas <github@timgthomas.com>
2024-12-11 17:07:39 -0600

    return a 404 if no post is found (debugging vercel)

--- src/routes/(index)/+page.server.ts
+++ src/routes/(index)/+page.server.ts
@@ -1,7 +1,9 @@
+import { error } from '@sveltejs/kit'
 import Post from '$lib/server/models/post'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async () => {
-  const posts = await Post.getAll()
-  return { post: posts[0] }
+  const [post] = await Post.getAll()
+  if (!post) error(404, 'Not found')
+  return { post }
 }
