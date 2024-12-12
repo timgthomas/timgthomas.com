af405ba524b93624bbf997b19654bae6192a02fc
Tim G. Thomas <github@timgthomas.com>
2024-12-12 00:23:01 -0600

    tidy up a bit

--- src/routes/[_year]/[_month]/[slug]/+page.server.ts
+++ src/routes/[_year]/[_month]/[slug]/+page.server.ts
@@ -1,7 +1,8 @@
 import { error, redirect } from '@sveltejs/kit'
 import Post from '$lib/server/models/post'
+import type { PageServerLoad } from './$types'
 
-export const load = async ({ params }) => {
+export const load: PageServerLoad = async ({ params }) => {
   const post = await Post.get(params.slug)
   if (!post) error(404, 'Not found')
   redirect(301, `/blog/${post.slug}`)
