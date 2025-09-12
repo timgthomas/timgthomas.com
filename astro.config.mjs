c15d29079b922bcc8a2daf7f937796f850083d06
Tim G. Thomas <github@timgthomas.com>
2025-09-12 11:39:18 -0500

    open external links in posts in new tabs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import inspectUrls from '@jsdevtools/rehype-url-inspector'
 import { defineConfig } from 'astro/config'
 import robots from 'astro-robots'
+
+const openLinksInNewTab = [
+  inspectUrls,
+  {
+    selectors: ['a[href]'],
+    inspectEach(url) {
+      if (!url.node.properties.href.includes('//')) return
+      url.node.properties.rel = 'noopener'
+      url.node.properties.target = '_blank'
+    },
+  },
+]
 
 export default defineConfig({
   site: 'https://timgthomas.com',
   integrations: [mdx(), robots(), sitemap()],
   markdown: {
+    rehypePlugins: [openLinksInNewTab],
     syntaxHighlight: 'prism',
   },
 })
